f16d8bb948d55f04929849337f65ebd0e87dee96
NickA02 <69744811+NickA02@users.noreply.github.com>
2023-03-26 16:15:46 -0400

    Streamline data paths for load_data

--- Model/data_loading.py
+++ Model/data_loading.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 
-def load_data(file_table_pth: str, base_path: str = "") -> pd.DataFrame:
+def load_data(file_table_pth: str) -> pd.DataFrame:
     """Given a table containing file names 
     returns compiled dataframe containing text bodies 
     and abstract bodies"""
@@ -14,8 +14,8 @@ def load_data(file_table_pth: str, base_path: str = "") -> pd.DataFrame:
 
     for row in paths.itertuples():
         try:
-            with open(f"{base_path}/{row['body']}") as body:
-                with open(f"{base_path}/{row['abstract']}") as abstract:
+            with open(f"/Data_Fetching/articles/{row['raw_path']}") as body:
+                with open(f"/Data_Fetching/abstracts/{row['abstract_path']}") as abstract:
                     df = pd.DataFrame({'id': [row['body'][:-6]], 'body': [body.read()], 'abstract': [abstract.read()]})
                     texts = pd.concat([texts,df])
                 print(i)
@@ -24,3 +24,17 @@ def load_data(file_table_pth: str, base_path: str = "") -> pd.DataFrame:
             raise Exception(f"File path(s) for ID {row['body'][:-6]} not found, or error in opening...")
 
     return texts
+
+def save_dataset(data: pd.Dataframe, name: str) -> str:
+    """
+    Arguments:
+        Data: Pandas Dataframe that contains data {id, body, abstract}
+    Returns:
+        String of compiled CSV path
+    """
+    
+
+
+
+
+        
